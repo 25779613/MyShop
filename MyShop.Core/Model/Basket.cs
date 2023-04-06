c84dbf51f944b958ac8ab45867078f34db8c722c
Salman Dukanwala <sdukanwala@iqbusiness.net>
2023-04-06 08:54:53 +0200

    12.BasketModel

---
+++ MyShop.Core/Model/Basket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Model
+{
+    public class Basket : BaseEntity
+    {
+        //virtual loads all the data from the database
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();
+        }
+
+    }
+}
--- MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop.DataAccess.SQL/DataContext.cs
@@ -10,9 +10,15 @@ namespace MyShop.DataAccess.SQL
 {
     public class DataContext : DbContext
     {
-        public DataContext(): base("DefaultConnection") { }
+        // the create the connection and tables
+        public DataContext() : base("DefaultConnection") { }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
+
+        //after adding tables use ef migration - Add-Migration Name & change the default project name to the file containing the data context
+        // update the db - Update-Database
     }
 
    
